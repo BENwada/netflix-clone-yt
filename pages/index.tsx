0037a00c9d868f37f944554b4b1b938c7e04cb87
BENwada <domo0103wada@gmail.com>
2022-11-02 19:20:52 +0900

    ,

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,8 +32,11 @@ const Home = ({
 }: Props) => {
   const { loading } = useAuth();
   const showModal = useRecoilValue(modalState);
+  const subscription = false;
 
-  if (loading) return null;
+  if (loading || subscription === null) return null;
+
+  if (!subscription) return <div>Plans</div>;
 
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
